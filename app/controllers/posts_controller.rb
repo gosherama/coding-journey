50083fef449851f6c0ea17c767491017bd3ee0ee
Gosherama <macciak@gmail.com>
2017-10-20 10:28:34 +0200

    Added Act_as_taggable_on

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,11 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
+    if params[:tag]
+      @posts = Post.tagged_with(params[:tag])
+    else
     @posts = Post.all
+    end
   end
 
   # GET /posts/1
@@ -66,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body, :tag_list)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
     resources :comments
   end
   
+  get 'tags/:tag', to: 'posts#index', as: :tag
+  
   root to: "posts#index"
+  
+  
 end
